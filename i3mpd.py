3295e46aab7216b219d5ee2a9fc737d9e8a26ba0
electra13x7777 <alex.f.barney@gmail.com>
2019-02-25 11:46:39 -0500

    adds Album and Song formatting for strings that are large.

--- i3mpd.py
+++ i3mpd.py
@@ -10,12 +10,33 @@ import dbus
 import os
 import sys
 
+# Format Album - Song
+#
+# @params: int albumlen, int songlen, string album, string song
+# Description: Formats album and song strings to given length. The Artist will not be reformatted.
+def format_str(max_album_len, max_song_len, album, song):
+    # assign len vars
+    albumlen = len(album)
+    songlen = len(song)
+    # check if strings are already formatted to desired lengths
+    if(albumlen <= max_album_len and songlen <= max_song_len):
+        return str(album + ' - ' + song)
+    # check if album is too long
+    elif(albumlen > max_album_len and songlen <= max_song_len):
+        return str(album[:max_album_len] + '...' + ' - ' + song)
+    # check if song is too long
+    elif(albumlen <= max_album_len and songlen > max_song_len):
+        return str(album + ' - ' + song[:max_song_len] + '...')
+    else:
+        return str(album[:max_album_len] + '...' + ' - ' + song[:max_song_len] + '...')
+
 try:
     bus = dbus.SessionBus()
 
     # Spotify Player
     spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
-
+    spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
+    props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
     if os.environ.get('BLOCK_BUTTON'):
         control_iface = dbus.Interface(spotify, 'org.mpris.MediaPlayer2.Player')
@@ -26,8 +47,6 @@ try:
         elif (os.environ['BLOCK_BUTTON'] == '3'):
             control_iface.Next()
 
-    spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
-    props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
     # Update PlaybackStatus
     pbstate = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
@@ -40,16 +59,12 @@ try:
     else:
         pbtn = ' (⏹) '
 
-    # Construct Position Bar
-    #songlen = probs['mpris:Track_Id']
-    #pos = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Position')
-
     # Wrapping Text
-    text = str(props['xesam:album']) + ' - ' + str(props['xesam:title'])
-    #sfile = open("~/.config/i3blocks/i3mpd.txt", 'r+')
-    #n = int(sfile.readline())
-    #sfile.close()
-    aas = str(props['xesam:artist'][0]) + ' : ' + text
+    album = str(props['xesam:album'])
+    song = str(props['xesam:title'])
+
+    format_str(20, 25, album, song)
+    aas = str(props['xesam:artist'][0]) + ' : ' + format_str(20, 25, album, song)
 
     if (sys.version_info > (3, 0)):
         print('❲ ⏮ ' + pbtn + '⏭ ❳  ' + aas)
